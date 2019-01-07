7e65a5b082ed5fae5211ae30073a26ecefee39bc
江守超 <jiangshouchao@58ganji.com>
2019-01-07 16:57:24 +0800

    file 2

---
+++ file2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func a() {
+
+    echo a
+}
+
+func b() {
+    echo b
+
+}
+
+func d() {
+}
+    
+func c() {
+
+    echo c
+}
